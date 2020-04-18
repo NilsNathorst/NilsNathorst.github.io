687429ed06e22682b1aae80b61de43ecf6132ad2
Nils <nilsnathorstwindahl@gmail.com>
2020-04-18 09:39:38 +0200

    init

---
+++ build/precache-manifest.753745d21984379721fbe78720f875cb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f9ea62e06871d85025b0dead924cde5a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e7eddbc6ed7f10ce8eb9",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "b6253b1a54e1f7e56caa",
+    "url": "/static/js/2.061ebcf8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.061ebcf8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e7eddbc6ed7f10ce8eb9",
+    "url": "/static/js/main.11b9c424.chunk.js"
+  },
+  {
+    "revision": "67770c4522434b70d8f5",
+    "url": "/static/js/runtime-main.7c38ce0a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
